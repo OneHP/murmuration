5ef5280ecbcbf527e02b48594eaf9db5f30385a6
Thomas Inman <onehitpoint@gmail.com>
2012-02-04 15:49:21 +0000

    Random turning in bird movement

--- src/domain/Bird.java
+++ src/domain/Bird.java
@@ -1,45 +1,75 @@
 package domain;
 
+import java.util.Random;
+
 import com.jme3.material.Material;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Line;
 
-public class Bird extends Geometry{
+public class Bird {
 
 	private static final float BIRD_TOP_SPEED = 20;
-	
-	private Vector3f location;
+	private static final float BIRD_TURN_SPEED = 8;
+	private static final float BIRD_TURN_CHANCE = 0.5f;
+	private static final float BIRD_SWITCH_CHANCE = 0.999f;
+
+	private final Vector3f location;
 	private Vector3f direction;
-	private float speed;
-	private Geometry geometry;
-	
-	public Bird(Vector3f location, Vector3f direction, float speed, Material material) {
+	private final float speed;
+	private final Geometry geometry;
+	private final Random random;
+	private float turnDirection;
+
+	public Bird(Vector3f location, Vector3f direction, float speed,
+			Material material) {
 		super();
 		this.location = location;
 		this.direction = direction;
 		this.speed = speed;
-		this.geometry = new Geometry("bird", makeView());
-		this.geometry.setMaterial(material);
+		geometry = new Geometry("bird", makeView());
+		geometry.setMaterial(material);
+		random = new Random();
+		turnDirection = 1;
 	}
-	
-	private Line makeView(){
+
+	private Line makeView() {
 		return new Line(location, tailLocation());
 	}
-	
-	private Vector3f tailLocation(){
+
+	private Vector3f tailLocation() {
 		float x = location.x - direction.x;
 		float y = location.y - direction.y;
-		return new Vector3f(x,y,0);
+		return new Vector3f(x, y, 0);
 	}
-	
-	public Geometry getGeometry(){
-		return this.geometry;
+
+	public Geometry getGeometry() {
+		return geometry;
 	}
-	
-	public void update(float tpf){
-		Vector3f movement = direction.mult(tpf*BIRD_TOP_SPEED);
-		this.location.add(movement);
-		this.geometry.move(movement);
+
+	public void update(float tpf) {
+		turn(tpf);
+		move(tpf);
+	}
+
+	private void turn(float tpf) {
+		float r = random.nextFloat();
+		if (r > BIRD_TURN_CHANCE) {
+			float turnValue = BIRD_TURN_SPEED * r * turnDirection * tpf;
+			Quaternion quaternion = new Quaternion(new float[] { 0, 0,
+					turnValue });
+			geometry.rotate(quaternion);
+			direction = quaternion.mult(direction);
+			if (r > BIRD_SWITCH_CHANCE) {
+				turnDirection *= -1;
+			}
+		}
+	}
+
+	private void move(float tpf) {
+		Vector3f movement = direction.mult(tpf * BIRD_TOP_SPEED);
+		location.add(movement);
+		geometry.move(movement);
 	}
 }
--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -8,53 +8,59 @@ import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
 
 import domain.Bird;
 
-/**
- * test
- * @author normenhansen
- */
 public class Main extends SimpleApplication {
 
 	private List<Bird> birds;
-	
-    public static void main(String[] args) {
-        Main app = new Main();
-        app.start();
-    }
 
-    @Override
-    public void simpleInitApp() {
-    	
-    	Vector3f up = new Vector3f(0,1,0);
-    	this.getCamera().lookAt(Vector3f.ZERO, up);
-    	
-    	this.mouseInput.setCursorVisible(true);
-        this.inputManager.clearMappings();
-        
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.White);
-    	
-        birds = new ArrayList<>();
-        Bird testBird = new Bird(Vector3f.ZERO, new Vector3f(0.1f, 0, 0), 0, mat);
-        birds.add(testBird);
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+	}
 
-        for(Bird bird : birds){
-        	rootNode.attachChild(bird.getGeometry());
-        }
-    }
+	@Override
+	public void simpleInitApp() {
 
-    @Override
-    public void simpleUpdate(float tpf) {
-    	 for(Bird bird : birds){
-    		 bird.update(tpf);
-    	}
-    }
+		Vector3f up = new Vector3f(0, 1, 0);
+		this.getCamera().lookAt(Vector3f.ZERO, up);
 
-    @Override
-    public void simpleRender(RenderManager rm) {
-        //TODO: add render code
-    }
+		mouseInput.setCursorVisible(true);
+		inputManager.clearMappings();
+
+		Material mat = new Material(assetManager,
+				"Common/MatDefs/Misc/Unshaded.j3md");
+		mat.setColor("Color", ColorRGBA.White);
+
+		birds = new ArrayList<>();
+		Bird testBird = new Bird(Vector3f.ZERO, new Vector3f(0.1f, 0, 0), 0,
+				mat);
+		birds.add(testBird);
+		Bird testBird2 = new Bird(Vector3f.ZERO, new Vector3f(0.1f, 0, 0), 0,
+				mat);
+		birds.add(testBird2);
+		Bird testBird3 = new Bird(Vector3f.ZERO, new Vector3f(0.1f, 0, 0), 0,
+				mat);
+		birds.add(testBird3);
+		Bird testBird4 = new Bird(Vector3f.ZERO, new Vector3f(0.1f, 0, 0), 0,
+				mat);
+		birds.add(testBird4);
+
+		for (Bird bird : birds) {
+			rootNode.attachChild(bird.getGeometry());
+		}
+	}
+
+	@Override
+	public void simpleUpdate(float tpf) {
+		for (Bird bird : birds) {
+			bird.update(tpf);
+		}
+	}
+
+	@Override
+	public void simpleRender(RenderManager rm) {
+		// TODO: add render code
+	}
 }
